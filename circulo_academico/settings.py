f59040a78f420bfb0173d1b7a5d7ee727cbb6dc4
Alexis2207R <22ruiz07@gmail.com>
2023-06-12 08:59:37 -0500

    static configurado

--- circulo_academico/settings.py
+++ circulo_academico/settings.py
@@ -240,7 +240,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'core/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static')
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
